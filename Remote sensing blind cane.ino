0e89147eb7579faabdcbd34b581e5c13d8e2c8af
ENG/ Reem Mohammed <reemmohammed2006n@gmail.com>
2026-08-06 03:44:13 +0300

    Add files via upload

---
+++ Remote sensing blind cane.ino
@@ -0,0 +1,61 @@
+const int trigPin = 9;
+const int echoPin = 8;
+const int buzzerPin = 10;
+
+long duration;
+int distance;
+
+const int dangerZone = 30;
+const int warningZone = 100;
+
+void setup()
+{
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(buzzerPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH, 30000);
+
+    if (duration == 0)
+    {
+        distance = warningZone + 1;
+    }
+    else
+    {
+        distance = duration * 0.034 / 2;
+    }
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    if (distance <= dangerZone && distance > 0)
+    {
+        tone(buzzerPin, 1000);
+        delay(50);
+    }
+    else if (distance > dangerZone && distance <= warningZone)
+    {
+        int delayTime = map(distance, dangerZone, warningZone, 50, 400);
+
+        tone(buzzerPin, 1000);
+        delay(50);
+        noTone(buzzerPin);
+        delay(delayTime);
+    }
+    else
+    {
+        noTone(buzzerPin);
+        delay(50);
+    }
+}
